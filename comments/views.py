43d3e5a5267d7959dd5ff29e67c2ad0ddec87454
FRNBDN <francesco.badini94@gmail.com>
2023-01-18 14:55:16 +0000

    add filter to comments serailizers

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwner
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -12,6 +13,12 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [IsOwner]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'item'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
