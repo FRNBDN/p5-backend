958e0e9d9abe89bd3d6d3f0ad5beb47e01b73374
FRNBDN <francesco.badini94@gmail.com>
2023-01-23 17:23:38 +0000

    Revert "fix drf_api views.py typo"
    This reverts commit 95151558ffcb981ff9753b65f441211e4bc1014e.

--- drf_api/views.py
+++ drf_api/views.py
@@ -2,18 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
+    JWT_AUTH_SECURE
 )
 
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to FRNBDNs drf API!"
+        'messsage': "FRNBDN's DRF_API for Productivity App"
     })
 
 
-# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
@@ -21,7 +20,7 @@ def logout_route(request):
         key=JWT_AUTH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires='Thu, 01 Jan 1970 00:00:00 GTM',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
         secure=JWT_AUTH_SECURE,
@@ -30,7 +29,7 @@ def logout_route(request):
         key=JWT_AUTH_REFRESH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires='Thu, 01 Jan 1970 00:00:00 GTM',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
         secure=JWT_AUTH_SECURE,
